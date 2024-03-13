a173e7f864da79df9b21c887a1a6c510df63af32
kjh5848 <kjh5848@gmail.com>
2024-03-13 14:07:58 +0900

    완료

--- src/main/java/shop/mtcoding/blog/board/BoardPersistRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardPersistRepository.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @RequiredArgsConstructor

ede827267882465c0da2d74b1ba2fe98783933d0
kjh5848 <kjh5848@gmail.com>
2024-03-15 11:15:44 +0900

    빌터패턴이해하기

---
+++ src/User.java
@@ -0,0 +1,84 @@
+public class User {
+
+    //빌더 패턴
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    public static void main(String[] args) {
+        User user = User.bulider()
+                .id(1)
+                .username("ssar")
+                .password("1234")
+                .email("ssar@nate.com");
+
+        System.out.println(user);
+    }
+
+    public static User bulider() {
+        return new User();
+    }
+
+    public User id(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public User username(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public User password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public User email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

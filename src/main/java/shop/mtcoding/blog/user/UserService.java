4fb91d3764bb469256720fc51230445f70f778f0
kjh5848 <kjh5848@gmail.com>
2024-03-19 12:43:31 +0900

    UserService 생성완료

---
+++ src/main/java/shop/mtcoding/blog/user/UserService.java
@@ -0,0 +1,9 @@
+package shop.mtcoding.blog.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}

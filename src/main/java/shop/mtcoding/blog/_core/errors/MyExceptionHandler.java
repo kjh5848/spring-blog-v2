039ccd8854c65011236ec371396cad9134f14cb4
kjh5848 <kjh5848@gmail.com>
2024-03-20 12:09:00 +0900

    에러 핸들러 완료

--- src/main/java/shop/mtcoding/blog/_core/errors/MyExceptionHandler.java
+++ src/main/java/shop/mtcoding/blog/_core/errors/MyExceptionHandler.java
@@ -1,42 +1,46 @@
 package shop.mtcoding.blog._core.errors;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.blog._core.errors.exception.*;
+import shop.mtcoding.blog._core.utills.ApiUtil;
 
 //RuntimeException이 터지면 해당 파일로 오류가 모인다.
-@ControllerAdvice
+@RestControllerAdvice
 public class MyExceptionHandler {
 
+    //@ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception400.class)
-    public String ex400(RuntimeException e, HttpServletRequest req) {
-        req.setAttribute("msg",e.getMessage());
-        return "err/400";
+    public ResponseEntity<?> ex400(RuntimeException e) {
+        ApiUtil<?> apiUtil = new ApiUtil<>(400,e.getMessage());// http body -> 구성한 객체
+        return new ResponseEntity<>(apiUtil, HttpStatus.BAD_REQUEST); // http body, header
     }
 
     @ExceptionHandler(Exception401.class)
-    public String ex401(RuntimeException e, HttpServletRequest req) {
-        req.setAttribute("msg",e.getMessage());
-        return "err/401";
+    public ResponseEntity<?> ex401(RuntimeException e) {
+        ApiUtil<?> apiUtil = new ApiUtil<>(401,e.getMessage());// http body -> 구성한 객체
+        return new ResponseEntity<>(apiUtil, HttpStatus.UNAUTHORIZED ); // http body, header
     }
 
     @ExceptionHandler(Exception403.class)
-    public String ex403(RuntimeException e, HttpServletRequest req) {
-        req.setAttribute("msg",e.getMessage());
-        return "err/403";
+    public ResponseEntity<?> ex403(RuntimeException e) {
+        ApiUtil<?> apiUtil = new ApiUtil<>(403,e.getMessage());// http body -> 구성한 객체
+        return new ResponseEntity<>(apiUtil, HttpStatus.FORBIDDEN ); // http body, header
     }
 
+
     @ExceptionHandler(Exception404.class)
-    public String ex404(RuntimeException e, HttpServletRequest req) {
-        req.setAttribute("msg",e.getMessage());
-        return "err/404";
+    public ResponseEntity<?> ex404(RuntimeException e) {
+        ApiUtil<?> apiUtil = new ApiUtil<>(404,e.getMessage());// http body -> 구성한 객체
+        return new ResponseEntity<>(apiUtil, HttpStatus.NOT_FOUND); // http body, header
     }
 
     @ExceptionHandler(Exception500.class)
-    public String ex500(RuntimeException e, HttpServletRequest req) {
-        req.setAttribute("msg",e.getMessage());
-        return "err/500";
+    public ResponseEntity<?> ex500(RuntimeException e) {
+        ApiUtil<?> apiUtil = new ApiUtil<>(500,e.getMessage());// http body -> 구성한 객체
+        return new ResponseEntity<>(apiUtil, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     //널포인트 익셉션
